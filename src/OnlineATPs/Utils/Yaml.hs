92daf1667347ecf7457f164c1c8a95e18b2d52a7
Jonathan Prieto-Cubides <jprieto9@eafit.edu.co>
2016-08-25 12:21:58 -0500

    new module to deal with Yaml files

---
+++ src/OnlineATPs/Utils/Yaml.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
+
+-- | Some utils functions to process the yaml files
+
+module OnlineATPs.Utils.Yaml
+  ( module YAP
+  , (.:.)
+  , (.?.)
+  , (.@.)
+  , hypen
+  , loadYAML
+  , lower
+  , underscore
+  , upper
+  ) where
+
+import           Control.Applicative ((<|>))
+import           Data.Aeson          as YAP (withObject)
+import           Data.Aeson.Types    as YAP
+import           Data.Text           as T
+import           Data.Yaml           as YAP
+import           Data.Yaml.Include   as YamlInclude
+
+
+underscore ∷ T.Text → T.Text
+underscore field = T.pack $ camelTo2 '_' $ T.unpack field
+
+hypen ∷ T.Text → T.Text
+hypen field = T.pack $ camelTo2 '-' $T.unpack field
+
+lower ∷ T.Text → T.Text
+lower = T.toLower
+
+upper ∷ T.Text → T.Text
+upper = T.toUpper
+
+(.?.) :: FromJSON a => Object  → T.Text → Parser (Maybe a)
+x .?. field = x .:? field
+  <|> x .:? underscore field
+  <|> x .:? hypen field
+  <|> x .:? lower field
+  <|> x .:? upper field
+
+(.:.) :: FromJSON a => Object  → T.Text → Parser a
+x .:. field = x .: field
+  <|> x .: underscore field
+  <|> x .: hypen field
+  <|> x .: lower field
+  <|> x .: upper field
+
+
+(.@.) ∷ FromJSON a ⇒ [Object] → T.Text → Parser a
+[]  .@. _ = fail  "failed. Expected at least one key-value"
+[x] .@. field = x .:. field
+(x:xs) .@. field = do
+  value ← x .?. field
+  maybe (xs .@. field) return value
+
+loadYAML ∷ FilePath → IO (Maybe Object)
+loadYAML path = do
+  decoded ← YamlInclude.decodeFileEither path
+  case decoded of
+    Right o   → return $ Just o
+    Left msg  → do
+      print msg
+      return Nothing
