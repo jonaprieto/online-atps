7116b77f6c96296ae33c2c6b9f077bc416ed36ac
Jonathan Prieto <prieto.jona@gmail.com>
2016-08-30 16:36:25 -0500

    preparing to merge into master

--- src/OnlineATPs/Utils/Version.hs
+++ src/OnlineATPs/Utils/Version.hs
@@ -7,23 +7,23 @@
 module OnlineATPs.Utils.Version ( progNameVersion ) where
 
 
--- import           Data.Char          (toUpper)
--- import           Data.Version       (showVersion)
--- import           Paths_onlineatps   (version)
--- import           System.Environment (getProgName)
+ import           Data.Char          (toUpper)
+ import           Data.Version       (showVersion)
+ import           Paths_onlineatps   (version)
+ import           System.Environment (getProgName)
 
 
--- toUpperFirst ∷ String → String
--- toUpperFirst []       = []
--- toUpperFirst (x : xs) =  toUpper x : xs
+ toUpperFirst ∷ String → String
+ toUpperFirst []       = []
+ toUpperFirst (x : xs) =  toUpper x : xs
 
 -- Uncomment this in order to use ghcid with Main.hs
 
-progNameVersion ∷ IO String
-progNameVersion = return "dev"
+--progNameVersion ∷ IO String
+--progNameVersion = return "dev"
 
 -- | Return program name and version information.
--- progNameVersion ∷ IO String
--- progNameVersion = do
---   progName ← getProgName
---   return $ toUpperFirst progName ++ " version " ++ showVersion version
+ progNameVersion ∷ IO String
+ progNameVersion = do
+   progName ← getProgName
+   return $ toUpperFirst progName ++ " version " ++ showVersion version
